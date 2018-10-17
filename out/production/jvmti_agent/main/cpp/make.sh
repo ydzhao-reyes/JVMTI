a3f50bfcb1e6cc29982ec41dd72bbbbaf624fad1
reyes <reyes@fanruan.com>
2018-10-17 17:48:53 +0800

    thread

--- out/production/jvmti_agent/main/cpp/make.sh
+++
@@ -1,1 +0,0 @@
-g++ -fPIC -shared Test.cpp -I$JAVA_HOME/include -I$JAVA_HOME/include/darwin -Wall -Wno-deprecated  -o libtestagent.so
--- src/main/cpp/makeC.sh
+++
@@ -1,1 +0,0 @@
-gcc -fPIC -shared replaceAgent.c -I$JAVA_HOME/include -I$JAVA_HOME/include/darwin -Wall -Wno-deprecated  -o libreplaceAgent.so
